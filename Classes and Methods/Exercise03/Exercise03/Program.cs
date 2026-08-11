68d05e6aeffd2b9f452cf650d1f0939f946b021b
Renan Telini <renan_telini@hotmail.com>
2026-08-11 19:13:49 -0300

    Added folder 'Classes and Methods' - Exercise03

---
+++ Classes and Methods/Exercise03/Exercise03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Rectangle rectangle = new Rectangle();
+
+            Console.WriteLine("Enter the height and width of a rectangle:");
+            rectangle.Height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            rectangle.Width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Area = {0}", rectangle.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimeter = {0}", rectangle.Perimeter().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal = {0}", rectangle.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
